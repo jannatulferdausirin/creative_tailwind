1f945a4b78ad3b6070e8a09e74d1efb84279aab4
jannatulferdous <jannatulferdausirin1@gmail.com>
2023-09-14 13:17:21 +0300

    first commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,36 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      // font family start here
+      fontFamily: {
+        'primaryFonts': ['Open Sans', 'sans - serif'],
+        'secondaryFonts': ['Paprika', ' cursive'],
+        'thirdFonts': ['Nunito', 'sans-serif'],
+        'forthFonts': ['Quicksand',' sans-serif'],
+      },
+      // color code start here
+      colors: {
+        'dark-violet': '#160C6D',
+        'medium-violet': '#48409C',
+        'light-violet': '#6B62C5',
+        'dark-green': '#1BBF00',
+        'dark': '#141135',
+        'gray': '#726E9E',
+        'white': '#FAFAFE',
+        'offWhite': '#EFF4FA',
+        
+      },
+          // background-images start here
+             backgroundImage: {
+        'bannerWhy': "url('../images/bg (12).png')",
+        'youMay': "url('../images/bg (11).png')",
+        'banner': "url('../images/bg (5).png')",
+        // 'bannerWhy': "url('../images/bg (12).png')",
+        // 'bannerWhy': "url('../images/bg (12).png')",
+        // 'bannerWhy': "url('../images/bg (12).png')",
+        
+      }    
+    },
+       
+  }
+}
